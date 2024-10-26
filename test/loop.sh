fdb6f242ae1b33395c0ebece1db4b65708e6b485
Jesse Stimpson <jesse.stimpson@gmail.com>
2024-10-26 12:14:39 -0400

    binding tester

---
+++ test/loop.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set +e
+for round in {1..10}
+do
+    echo "Round $round"
+    echo "========"
+    echo ""
+
+    echo "# scripted"
+    foundationdb/bindings/bindingtester/bindingtester.py erlang --test-name scripted || exit 1
+
+    echo "# api"
+    foundationdb/bindings/bindingtester/bindingtester.py erlang --test-name api --num-ops 1000 --compare python || exit 1
+
+    echo "# api with concurrency 5"
+    foundationdb/bindings/bindingtester/bindingtester.py erlang --test-name api --num-ops 1000 --concurrency 5 || exit 1
+
+    echo "# directory"
+    foundationdb/bindings/bindingtester/bindingtester.py erlang --test-name directory --num-ops 1000 --compare || exit 1
+
+    echo "# directory hca"
+    foundationdb/bindings/bindingtester/bindingtester.py erlang --test-name directory_hca --num-ops 100 --concurrency 5 || exit 1
+done
--- test/tester.es
+++ test/tester.es
@@ -555,6 +555,14 @@ execute(TxObj, St, <<"GET_RANGE_SELECTOR">>) ->
     stack_push_range(St, Resp, Prefix),
     St;
 
+execute(TxObj, St, <<"GET_RANGE_SPLIT_POINTS">>) ->
+    [Key1, Key2, ChunkSize] = stack_pop(St, 3),
+    Options = [{chunk_size, ChunkSize}],
+    Result = erlfdb:get_range_split_points(TxObj, Key1, Key2, Options),
+    true = is_list(erlfdb:wait(Result)),
+    stack_push(St, <<"GOT_RANGE_SPLIT_POINTS">>),
+    St;
+
 execute(TxObj, St, <<"GET_READ_VERSION">>) ->
     LastVersion = erlfdb:wait(erlfdb:get_read_version(TxObj)),
     stack_push(St, <<"GOT_READ_VERSION">>),
